405bd71f5a4ad635f00958cf5607c5b43e1ebe28
Tashfia <tashfiahasan300@gmail.com>
2020-07-29 16:07:27 +0000

    index1 home page

--- app.py
+++ app.py
@@ -106,6 +106,10 @@ cities = {
 }
 # -- Routes section --
 @app.route('/')
+@app.route('/index1')
+def index_one():
+    return render_template('index1.html')
+@app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
